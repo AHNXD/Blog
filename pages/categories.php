41220c6aa50bf0f4bc51269949181de37beadd9e
Rama-Youssef <rama.youssef.333@gmail.com>
2024-01-09 14:26:55 -0800

    change

--- pages/categories.php
+++ pages/categories.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
  include("connection.php");
  if ($connection->connect_error) {
@@ -20,5 +28,6 @@
     }
 }
 ?>
-
+</body>
+</html>
 
