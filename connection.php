ac713a496b9cd1fc7925d2d82a7ca306d72c4588
Ali Al-Hadi Nizam <nzamlyalhady@gmail.com>
2024-01-09 12:32:11 +0300

    connection

--- connection.php
+++ connection.php
@@ -1,3 +1,10 @@
 <?php
+include ("connectionString.php");
+// Create connection
+$conn = new mysqli($servername, $username, $password, $port);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} echo "Connected successfully";
 ?>
---
+++ home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>test</title>
+</head>
+<body>
+    <?php
+        include ("connection.php");
+    ?>
+</body>
+</html>
