63b6de06e4dc33b2d396933d0ddeb7cd83f8ae36
Ali Al-Hadi Nizam <nzamlyalhady@gmail.com>
2024-01-10 14:32:12 +0300

    edit

--- pages/admin.php
+++ pages/admin.php
@@ -12,12 +12,17 @@
             <div class="form-inner">
                 <div class="title-admin"><h1><?php echo $_GET['user'] ?></h1></div>
                 <div class="btns">
-                    <button class="button-64" role="button"><span class="text">Categories</span></button>
-                    <button class="button-64" role="button"><span class="text">Add Article</span></button>
+                    <button class="button-64" role="button" onclick=""><span class="text">Categories</span></button>
+                    <button class="button-64" role="button" onclick="goToAddArticle()"><span class="text">Add Article</span></button>
                 </div>
                 <button class="button-85" role="button">LogOut</button>
             </div>
         </div>
     </div>
+    <script>
+        function goToAddArticle() {
+            window.location = "addArticle.php";
+        }
+    </script>
 </body>
 </html>
--- pages/categories.php
+++ pages/categories.php
@@ -7,26 +7,24 @@
 </head>
 <body>
 <?php
- include("connection.php");
- if ($connection->connect_error) {
-    die('Connection failed: ' . $connnection->connect_error);
+    include("../database/connection.php");
 
- $sql = "SELECT * FROM categories";
- $data = mysqli_query($connection, $sql);
+    $sql = "SELECT * FROM categories";
+    $data = mysqli_query($connection, $sql);
 
- if ($data->num_rows > 0) {
-    $rows = $data->fetch_all(MYSQLI_ASSOC);
-    for($i =0 ; $i<count($rows); $i++){
-        $title = $rows[$i]['category_name'];
-        $imageURL = $rows[$i]['image_url'];
+    if ($data->num_rows > 0) {
+        $rows = $data->fetch_all(MYSQLI_ASSOC);
+        for($i =0 ; $i<count($rows); $i++){
+            $title = $rows[$i]['category_name'];
+            $imageURL = $rows[$i]['photo'];
 
-        // Display category image with text
-        echo '<div>';
-        echo '<img src="' . $imageURL . '" alt="' . $name . '">';
-        echo '<p>' . $name . '</p>';
-        echo '</div>';
+            // Display category image with text
+            echo '<div>';
+            echo '<img src="' . $imageURL . '" alt="' . $name . '">';
+            echo '<p>' . $name . '</p>';
+            echo '</div>';
+        }
     }
-}
 ?>
 </body>
 </html>
