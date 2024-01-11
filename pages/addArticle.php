ae7042a703d82dd8710c1491de801b12bfda6f1b
Ali Al-Hadi Nizam <nzamlyalhady@gmail.com>
2024-01-11 18:27:34 +0300

    '

--- pages/addArticle.php
+++ pages/addArticle.php
@@ -34,7 +34,6 @@
                                     }
                                     $connection->close();
                                 ?>
-                                
                             </datalist>
                         </div>
                         <div class="field">
